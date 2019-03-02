de0dbb8a8cec2e5ace3ed2c8d8e514e9a1ea4652
[Kankarollo] <[pawel.kankowski@gmail.com]>
2019-03-02 23:17:49 +0100

    Zad 1

--- LAB_1/main.py
+++ LAB_1/main.py
@@ -1,64 +1,72 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-
+import matplotlib.patches as mpatches
 
 def main():
+   skrypt_jeden()
+   skrypt_dwa()
+
+
+
+def skrypt_jeden():
     x = 0.5
     for i in range(10):
-        wynik = rozwiniecie(i + 1, x)
+        wynik = rozwiniecie_poprawne(i + 1, x)
         print(wynik)
         roznica_wzgledne_bewzgledne(wynik)
 
-    wartosc1 = []
-    wartosc3 = []
-    wartosc10 = []
-    for i in range(100):
-        wartosc1.append(rozwiniecie(1, i))
-        wartosc3.append(rozwiniecie(3, i))
-        wartosc10.append(rozwiniecie(10, i))
+def skrypt_dwa():
+    x = np.arange(0.0, 1.0, 0.01)
+
+    wartosc1 = [rozwiniecie_poprawne(1, xi) for xi in x]
+    wartosc3 = [rozwiniecie_poprawne(3, xi) for xi in x]
+    wartosc10 = [rozwiniecie_poprawne(10, xi) for xi in x]
 
     wartosc1 = np.asarray(wartosc1)
     wartosc3 = np.asarray(wartosc3)
     wartosc10 = np.asarray(wartosc10)
 
-    i = list(range(100))
-    plt.plot(i, wartosc1)
-    plt.hold
-    plt.plot(i, wartosc3)
-    plt.hold
-    plt.plot(i, wartosc10)
+    plt.plot(x, wartosc1, 'r', x, wartosc3, 'b', x,wartosc10, 'g')
+    red_patch = mpatches.Patch(color='red', label='n = 1')
+    blue_patch= mpatches.Patch(color='blue', label='n = 3')
+    green_patch = mpatches.Patch(color='green', label='n = 10')
+    plt.legend(handles=[red_patch, blue_patch, green_patch])
     plt.show()
 
+def rozwiniecie_poprawne(n,x):
+    indexy = np.arange(n)
+    potega = indexy*2 + 1
+    licznik = np.abs(indexy*2-1)
+    licznik = np.cumprod(licznik)
+    mianownik1 = np.arange(1,2*n,2)
+    mianownik2 = np.arange(0,2*n,2)
+    mianownik2[0] = 1
+    mianownik = np.cumprod(mianownik2)*mianownik1
 
-def rozwiniecie(n, x):
-    licznik = np.array([x, (x ** 3) / (2 * 3), (3 * x ** 5) / (2 * 4 * 5), (15 * x ** 7) / (2 * 4 * 6 * 7),
-                        (3 * 5 * 7 * x ** 9) / (2 * 4 * 6 * 8 * 9),
-                        (1 * 3 * 5 * 7 * 9 * 11 * x ** 13) / (2 * 4 * 6 * 8 * 10 * 11),
-                        (1 * 3 * 5 * 7 * 9 * 11 * 13 * x ** 15) / (2 * 4 * 6 * 8 * 10 * 12 * 13),
-                        (1 * 3 * 5 * 7 * 9 * 11 * 13 * 15 * x ** 17) / (2 * 4 * 6 * 8 * 10 * 12 * 14 * 15),
-                        (1 * 3 * 5 * 7 * 9 * 11 * 13 * 15 * 17 * x ** 19) / (2 * 4 * 6 * 8 * 10 * 12 * 14 * 16 * 17),
-                        (1 * 3 * 5 * 7 * 9 * 11 * 13 * 15 * 17 * 19 * x ** 21) / (
-                                    2 * 4 * 6 * 8 * 10 * 12 * 14 * 16 * 18 * 19)])
-    arc_sin = np.sum(licznik[:n])
+    tablica_wynikow = (np.power(x,potega)*licznik)/mianownik
+
+    arc_sin = np.sum(tablica_wynikow)
     arc_cos = (math.pi / 2) - arc_sin
 
     return math.degrees(arc_cos)
 
 
+
 def roznica_wzgledne_bewzgledne(liczba):
-    wartosc_bewzgledna = np.double(math.fabs(liczba - 60.0))
-    wartosc_wzgledna = np.double(wartosc_bewzgledna / 60.0) * 100.0
+    wartosc_bewzgledna = math.fabs(liczba - 60.0)
+    wartosc_wzgledna = (wartosc_bewzgledna / 60.0) * 100.0
     print("Wartosc bewzgledna: ", wartosc_bewzgledna, "Wartosc wzgledna: ", wartosc_wzgledna, "%")
-    # print('Wartosc bewzgledna: %d Wartosc wzgledna: %s  ' % (wartosc_bewzgledna, wartosc_wzgledna))
 
 
 def test():
-    test = np.array([1, 2, 3, 4, 5])
-    print(np.sum(test[:3]))
-    print(math.degrees(math.pi))
+    x = np.array([1,2,3])
+    y = np.array([4,5,6])
+    print(np.multiply(x,y))
+    print(np.convolve(x,y))
 
 
 if __name__ == '__main__':
     main()
     # test()
+    # rozwiniecie_poprawne(5)
